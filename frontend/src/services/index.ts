5b6255f994c433c02e28f54a3598d02c6c86b0a0
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-05-25 16:19:14 +0800

    Add missing services index file to fix import errors

---
+++ frontend/src/services/index.ts
@@ -0,0 +1,13 @@
+// Service exports for easy importing
+export { default as adminService } from './adminService';
+export { default as announcementService } from './announcementService';
+export { default as assignmentService } from './assignmentService';
+export { default as authService } from './authService';
+export { default as courseService } from './courseService';
+export { default as forumService } from './forumService';
+export { default as notificationService } from './notificationService';
+export { default as uploadService } from './uploadService';
+export { default as userService } from './userService';
+
+// Re-export API instance for direct use
+export { api } from './api';
