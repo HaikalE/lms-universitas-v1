ab49b24830c5ea902485369a76fb209cc9816251
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-07-04 16:25:15 +0700

    📦 Update ForumsModule with all new forum entities

--- backend/src/forums/forums.module.ts
+++ backend/src/forums/forums.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ForumsService } from './forums.service';
 import { ForumsController } from './forums.controller';
-import { ForumPost } from '../entities/forum-post.entity';
-import { Course } from '../entities/course.entity';
-import { User } from '../entities/user.entity';
+import { ForumsService } from './forums.service';
+import { 
+  ForumPost, 
+  ForumLike, 
+  ForumAttachment, 
+  ForumNotification,
+  Course,
+  User
+} from '../entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ForumPost, Course, User])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ForumPost,
+      ForumLike,
+      ForumAttachment,
+      ForumNotification,
+      Course,
+      User,
+    ]),
+  ],
   controllers: [ForumsController],
   providers: [ForumsService],
   exports: [ForumsService],
