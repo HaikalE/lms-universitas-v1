37e001881fc8e7bf8b148ff8e433b0308bb4a688
Muhammad Haikal Rahman <89823572+HaikalE@users.noreply.github.com>
2025-08-02 06:50:34 +0700

    🔔 Enhance forums module with notification auto-triggers

--- backend/src/forums/forums.module.ts
+++ backend/src/forums/forums.module.ts
@@ -5,9 +5,13 @@ import { ForumsController } from './forums.controller';
 import { ForumPost } from '../entities/forum-post.entity';
 import { Course } from '../entities/course.entity';
 import { User } from '../entities/user.entity';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ForumPost, Course, User])],
+  imports: [
+    TypeOrmModule.forFeature([ForumPost, Course, User]),
+    NotificationsModule, // 🔔 Add notifications for auto-triggers
+  ],
   controllers: [ForumsController],
   providers: [ForumsService],
   exports: [ForumsService],
